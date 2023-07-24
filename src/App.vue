66cbd289f118e42aa696323ec9e3bb6544b7b801
Tapan Padaliya <tapan@7span.com>
2023-07-24 12:33:16 +0530

    init project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline text-red-400">Hello world!</h1>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
