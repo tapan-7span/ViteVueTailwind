b30c5dd02544a4959b7321ff1dcd7a7cf0eb46a8
Tapan Padaliya <tapan@7span.com>
2023-08-22 19:14:01 +0530

    captcha implement

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
-    <VideoPlayer />
+    <form>
+      <button
+        class="g-recaptcha"
+        data-sitekey="6Lf0iscnAAAAAPp6ZS__werzyBxCkFlLbvt_ZiiO"
+        data-callback="onSubmit"
+      >
+        Submit
+      </button>
+      <br />
+    </form>
   </div>
 </template>
 
 <script>
-import VideoPlayer from "./components/VideoPlayer.vue"; // Update the path based on your project structure
-
 export default {
-  components: {
-    VideoPlayer
-  }
+  methods: {
+    onSubmit(token) {
+      console.log("reCAPTCHA challenge completed successfully!");
+    },
+  },
+  mounted() {
+    // Load the reCAPTCHA script asynchronously
+    const script = document.createElement("script");
+    script.src = "https://www.google.com/recaptcha/api.js";
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+  },
 };
 </script>
---
+++ src/components/7span_video_player.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <VideoPlayer />
+  </div>
+</template>
+
+<script>
+import VideoPlayer from "./components/VideoPlayer.vue"; // Update the path based on your project structure
+
+export default {
+  components: {
+    VideoPlayer,
+  },
+};
+</script>
