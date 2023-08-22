855eeb595cca2960b56bb4b6c5ffce2f7b957765
Tapan Padaliya <tapan@7span.com>
2023-08-22 20:28:59 +0530

    user commitn

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,38 @@
 <template>
   <div>
-    <form @submit.prevent="onSubmit">
-      <button
-        ref="recaptchaButton"
+    <form @submit.prevent="submitForm">
+      <label for="name">Name:</label>
+      <input type="text" id="name" v-model="name" />
+
+      <div
         class="g-recaptcha"
         data-sitekey="6Lf0iscnAAAAAPp6ZS__werzyBxCkFlLbvt_ZiiO"
-        data-callback="submitForm"
+        data-callback="onReCaptchaSuccess"
         data-size="invisible"
-      >
-        Submit
-      </button>
+      ></div>
+
+      <button type="submit">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    onSubmit() {
-      // Trigger the reCAPTCHA challenge programmatically
-      this.executeRecaptcha();
-    },
-    executeRecaptcha() {
-      grecaptcha.ready(() => {
-        grecaptcha
-          .execute("6Lf0iscnAAAAAPp6ZS__werzyBxCkFlLbvt_ZiiO", {
-            action: "submit",
-          })
-          .then((token) => {
-            this.submitForm(token);
-          });
-      });
-    },
-    submitForm(token) {
-      // Your form submission logic goes here
-      // Include the reCAPTCHA token in your payload if needed
-      console.log("reCAPTCHA challenge completed successfully!");
-    },
+  data() {
+    return {
+      name: "",
+    };
   },
-  mounted() {
-    const script = document.createElement("script");
-    script.src = "https://www.google.com/recaptcha/api.js?render=explicit";
-    script.async = true;
-    script.defer = true;
-    document.head.appendChild(script);
+
+  methods: {
+    submitForm() {
+      grecaptcha.execute();
+    },
+
+    onReCaptchaSuccess(response) {
+      console.log("Name:", this.name);
+      console.log("reCAPTCHA Response:", response);
+    },
   },
 };
 </script>
