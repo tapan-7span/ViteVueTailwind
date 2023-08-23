a60d4cd724635a408699737d7d8e392adc3150f7
Tapan Padaliya <tapan@7span.com>
2023-08-23 10:40:23 +0530

    new exicution

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <form @submit.prevent="validate">
+    <form>
       Name: (required) <input v-model="name" />
       <div
         id="recaptcha"
         class="g-recaptcha"
-        :data-sitekey="recaptchaSiteKey"
+        :data-sitekey="siteKey"
         :data-callback="onSubmit"
         data-size="invisible"
       ></div>
-      <button id="submit">submit</button>
+      <button @click.prevent="validate">submit</button>
     </form>
   </div>
 </template>
@@ -19,7 +19,7 @@ export default {
   data() {
     return {
       name: "",
-      recaptchaSiteKey: "6Lf0iscnAAAAAPp6ZS__werzyBxCkFlLbvt_ZiiO", // Replace with your actual site key
+      siteKey: "6Lf0iscnAAAAAPp6ZS__werzyBxCkFlLbvt_ZiiO",
     };
   },
   methods: {
@@ -34,24 +34,13 @@ export default {
         grecaptcha.execute();
       }
     },
+    onload() {
+      var element = document.getElementById("submit");
+      element.onclick = this.validate;
+    },
   },
   mounted() {
-    const element = document.getElementById("submit");
-    element.addEventListener("click", this.validate);
-    this.$nextTick(() => {
-      const script = document.createElement("script");
-      script.src = "https://www.google.com/recaptcha/api.js";
-      script.async = true;
-      script.defer = true;
-      document.head.appendChild(script);
-      script.onload = () => {
-        grecaptcha.render("recaptcha", {
-          sitekey: this.recaptchaSiteKey,
-          callback: this.onSubmit,
-          size: "invisible",
-        });
-      };
-    });
+    this.onload();
   },
 };
 </script>
