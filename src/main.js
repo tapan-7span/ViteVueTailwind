3648f139d7b5311c5687bf6dd894383b335c2d4b
Tapan Padaliya <tapan@7span.com>
2023-09-14 15:34:29 +0530

    vlist changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,37 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import VList from "@7span/vue-list-v3";
+import axios from "axios";
+import qs from "qs";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(VList, {
+  requestHandler(data) {
+    const { endpoint, pagination, search, sort, filters } = data;
+    const { page, perPage } = pagination;
+    return axios
+      .get(endpoint, {
+        params: {
+          page,
+          per_page: perPage,
+          search,
+          sort_by: sort.by,
+          sort_order: sort.order,
+          filter: filters,
+        },
+        paramsSerializer: (params) => qs.stringify(params),
+      })
+      .then((res) => {
+        return {
+          items: res.data?.data,
+          count: res.data.meta.total,
+        };
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+});
+
+app.mount("#app");
